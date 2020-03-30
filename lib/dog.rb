07a824f106dec1bc4361669b5fdd4708adc583cf
Harish Manoharan <harishmanoharan17@gmail.com>
2020-03-30 13:00:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 # Your code goes here!
 class Dog
-  def .name=(name)
+  def name=(name)
     @name=name
   end
-  def .name
+  def name
     @name
   end
-  def .bark
+  def bark
     puts "woof!"
   end
 end
